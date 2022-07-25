0f426e6250ccf2814e1ceb3580bc2409b55ee4f7
ASUS <vstudio776@gmail.com>
2022-07-25 13:54:59 +0700

    initialized commit (first)

---
+++ app/src/main/java/com/alifidn/localization/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.alifidn.localization
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.alifidn.localization.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    /* private var _binding: ActivityMainBinding? = null
+    *  private val binding get() = _binding as ActivityMainBinding */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /* val btn = binding.btnme
+
+        btn.setOnClickListener {
+            Toast.makeText(this, "Beep", Toast.LENGTH_SHORT).show()
+        } */
+
+        binding.btnme.setOnClickListener {
+            val intent = Intent(this, OtherActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "Beep", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "Beep ${R.string.onStartInfo} Main", Toast.LENGTH_SHORT).show()
+        Log.d("Activity", "onStart Info Main")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "Beep ${R.string.onResumeInfo} Main", Toast.LENGTH_SHORT).show()
+        Log.d("Activity", "onResume Info Main")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "Beep ${R.string.onPauseInfo} Main", Toast.LENGTH_SHORT).show()
+        Log.d("Activity", "onPause Info Main")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "Beep ${R.string.onStopInfo} Main", Toast.LENGTH_SHORT).show()
+        Log.d("Activity", "onStop Info Main")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "Beep ${R.string.onDestroyInfo} Main", Toast.LENGTH_SHORT).show()
+        Log.d("Activity", "onDestroy Info Main")
+    }
+}
---
+++ app/src/main/java/com/alifidn/localization/OtherActivity.kt
@@ -0,0 +1,65 @@
+package com.alifidn.localization
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.alifidn.localization.databinding.ActivityMainBinding
+import com.alifidn.localization.databinding.ActivityOtherBinding
+
+class OtherActivity : AppCompatActivity() {
+
+    /* private lateinit var binding: ActivityMainBinding */
+
+    private var _binding: ActivityOtherBinding? = null
+    private val binding get() = _binding as ActivityOtherBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityOtherBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /* val btn = binding.btnmu
+
+        btn.setOnClickListener {
+            Toast.makeText(this, "Beep", Toast.LENGTH_SHORT).show()
+        } */
+
+        binding.btnmu.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "Boop", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "Boop ${R.string.onStartInfo} Other", Toast.LENGTH_SHORT).show()
+        Log.d("Activity", "onStart Info Other")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "Boop ${R.string.onResumeInfo} Other", Toast.LENGTH_SHORT).show()
+        Log.d("Activity", "onResume Info Other")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "Boop ${R.string.onPauseInfo} Other", Toast.LENGTH_SHORT).show()
+        Log.d("Activity", "onPause Info Other")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "Boop ${R.string.onStopInfo} Other", Toast.LENGTH_SHORT).show()
+        Log.d("Activity", "onStop Info Other")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "Boop ${R.string.onDestroyInfo} Other", Toast.LENGTH_SHORT).show()
+        Log.d("Activity", "onDestroy Info Other")
+    }
+}
